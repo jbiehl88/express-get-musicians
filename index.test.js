2e307f47bed250aa56aad34c4b860f6de221150f
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-09-23 11:58:01 -0500

    Complete - added get req/res and tests

--- index.test.js
+++ index.test.js
@@ -1,22 +1,39 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process")
+execSync("npm install")
+execSync("npm run seed")
 
 const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const { db } = require("./db/connection")
+const { Musician, Band } = require("./models/index")
+const app = require("./src/app")
+const { seedMusician } = require("./seedData")
 
+describe("./musicians endpoint", () => {
+	// Write your tests here
+	it("testing musicians endpoint", async () => {
+		const response = await request(app).get("/musicians")
+		expect(response.statusCode).toBe(200)
+	})
+	it("testing musicians properties", async () => {
+		const response = await request(app).get("/musicians")
+		const responseData = JSON.parse(response.text)
+		expect(responseData[0].name).toBe("Mick Jagger")
+		expect(responseData[1].instrument).toBe("Voice")
+		expect(responseData[2].id).toBe(3)
+	})
+})
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
+describe("Bands", () => {
+	it("testing bands endpoint", async () => {
+		const response = await request(app).get("/bands")
+		expect(response.statusCode).toBe(200)
+	})
+	it("testing bands properties", async () => {
+		const response = await request(app).get("/bands")
+		const responseData = JSON.parse(response.text)
+		expect(responseData[0].name).toBe("The Beatles")
+		expect(responseData[1].genre).toBe("Pop")
+		expect(responseData[2].id).toBe(3)
+	})
 })
--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
-const express = require("express");
-const app = express();
-const { Musician } = require("../models/index")
+const express = require("express")
+const app = express()
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
-const port = 3000;
+const port = 3000
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
 
+app.get("/musicians", async (req, res) => {
+	let foundMusicians = await Musician.findAll({})
+	res.json(foundMusicians)
+})
 
+app.get("/musicians/1", async (req, res) => {
+	let foundFirst = await Musician.findByPk(1)
+	res.json(foundFirst)
+})
 
+app.get("/musicians/2", async (req, res) => {
+	let foundSecond = await Musician.findByPk(2)
+	res.json(foundSecond)
+})
 
+app.get("/musicians/3", async (req, res) => {
+	let foundThird = await Musician.findByPk(3)
+	res.json(foundThird)
+})
 
+app.get("/bands", async (req, res) => {
+	let foundBands = await Band.findAll({})
+	res.json(foundBands)
+})
 
-
-module.exports = app;
+module.exports = app
