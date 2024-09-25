405927ea65c3fd44e537e2783b2c5efdff0054f8
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-09-25 11:44:03 -0500

    removed commented out old routes

--- src/app.js
+++ src/app.js
@@ -13,37 +13,9 @@ app.use(express.urlencoded())
 
 app.use("/musicians", musicianRouter)
 
-// app.get("/musicians", async (req, res) => {
-// 	let foundMusicians = await Musician.findAll({})
-// 	res.json(foundMusicians)
-// })
-
-// app.get("/musicians/:id", async (req, res) => {
-// 	let musicianId = req.params.id
-// 	let musician = await Musician.findByPk(musicianId)
-// 	res.json(musician)
-// })
-
 app.get("/bands", async (req, res) => {
 	let foundBands = await Band.findAll({})
 	res.json(foundBands)
 })
-
-// app.post("/musicians/new", async (req, res) => {
-// 	const createMusician = await Musician.create(req.body)
-// 	res.json(createMusician)
-// })
-
-// app.put("/musicians/:id", async (req, res) => {
-// 	const musicianId = req.params.id
-// 	const updateMusician = await Musician.update(req.body, { where: { id: musicianId } })
-// 	res.json(updateMusician)
-// })
-
-// app.delete("/musicians/:id", async (req, res) => {
-// 	const musicianId = req.params.id
-// 	const deleteMusician = await Musician.destroy({ where: { id: musicianId } })
-// 	res.json(deleteMusician)
-// })
 
 module.exports = app
